2aab17ba3989525fe3a3786672e2cdd001df78f9
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-06-03 04:49:15 +0530

    Added : Code to create a payroll service database

--- src/main/java/org/payrolljdbc/Main.java
+++ src/main/java/org/payrolljdbc/Main.java
@@ -1,8 +1,40 @@
 package org.payrolljdbc;
 
-public class Main {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Welcome to employee payroll service using JDBC");
+        /*
+         * Connection for connecting with the url,user and password
+         */
+        Connection connection = DriverManager.getConnection(Sys.url, Sys.user, Sys.password);
+        Statement statement = connection.createStatement();
+
+        /*
+         * Commends for creating database and use database
+         */
+        String createDatabase = "CREATE DATABASE payroll_service_JDBC";
+        String useDatabase = "USE payroll_service_JDBC";
+
+        statement.execute(createDatabase);
+        statement.execute(useDatabase);
+
+        // commands for delete database
+        String deleteDatabase = "DROP DATABASE payroll_service_JDBC";
+
+
+        System.out.print("Do you want to delete the database (Yes/No) - ");
+        String choice = new Scanner(System.in).next();
+
+        if (choice.equalsIgnoreCase("yes")) {
+            statement.execute(deleteDatabase);
+            System.out.println("database deleted successfully....");
+        }
+
     }
 }
---
+++ src/main/java/org/payrolljdbc/Sys.java
@@ -0,0 +1,7 @@
+package org.payrolljdbc;
+
+public class Sys {
+    public static String user="root";
+    public static String password="Masai@321";
+    public static String url= "jdbc:mysql://localhost:3306/";
+}
