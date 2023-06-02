695931d68b6e859afc0b3d3cf47d622acb6a5fbb
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-06-03 03:54:33 +0530

    Added : Welcome message

---
+++ src/main/java/org/payrolljdbc/Main.java
@@ -0,0 +1,8 @@
+package org.payrolljdbc;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to employee payroll service using JDBC");
+    }
+}
